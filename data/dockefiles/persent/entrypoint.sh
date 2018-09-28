67473c2835b5ef8b3b26d3b36c890cec5fe3bd1a
wang_yongzhen <wang_yongzhen@topsec.com.cn>
2018-09-28 15:33:54 +0800

    完善webhook和present的代码

--- data/dockefiles/persent/entrypoint.sh
+++ data/dockefiles/persent/entrypoint.sh
@@ -5,6 +5,6 @@ git clone ssh://git@git.cloud.top:222/vdfw/doc.git
 
 cd /root/doc
 
-nohup /usr/local/bin/present -orighost 172.19.22.3  -notes -http 0.0.0.0:3999 &
+nohup /usr/local/bin/webhook -hooks /root/gitlab.json & >/dev/null 2>&1
+exec /usr/local/bin/present -orighost 172.19.22.3  -notes -http 0.0.0.0:3999 >/dev/null 2>&1 
 
-exec /usr/local/bin/webhook -hooks /root/gitlab.json -ip 172.19.22.3 -port 9000
